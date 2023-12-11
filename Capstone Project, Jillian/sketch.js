1ae4bdfd6ed39383900fdda5e513cda37759f12e
Jillian06 <jillianwang16@gmail.com>
2023-12-11 15:21:45 -0600

    Use play to create the starting frame of the game and add Enter to start the game text,

--- Capstone Project, Jillian/sketch.js
+++ Capstone Project, Jillian/sketch.js
@@ -1,16 +1,47 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Nausicaa
+// Jillian
+// 2023.12.11
 
-//tryout
+let gameStart;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(2388, 1668);
+  gameStart = loadAni('gameStart/IMG_01.png', 21);
 }
 
 function draw() {
-  background(220);
+  clear();
+  // pressStart();
+  animation(gameStart, 1194, 834);
+  animation.time(3);
+  pressStart();
 }
+
+function pressStart(){
+  textSize(60);
+  fill(255);
+  stroke(0);
+  strokeWeight(4);
+  text('Press ENTER to Start the Game', 1350, 1660);
+}
+
+// function draw() {
+//   //background(0);
+//   animation(gameStart, 250, 80);
+//   //image(gameStart, mouseX,mouseY);
+// }
+
+
+// let cloudAni;
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   cloudAni = loadAni('gameStart/IMG_01.png', 9);
+// }
+
+// function draw() {
+//   background(220);
+//   clear();
+//   animation(cloudAni, width/2, height/2);
+// }
+
