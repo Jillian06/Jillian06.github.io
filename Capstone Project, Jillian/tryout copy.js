dabcf0031670282d37bf5592e7b87337cc68ade3
Jillian06 <jillianwang16@gmail.com>
2024-01-05 15:37:50 -0600

    2024.01.05
    Trying to make some changes for my new game.

---
+++ Capstone Project, Jillian/tryout copy.js
@@ -0,0 +1,134 @@
+// Nausicaa
+// Jillian
+// 2023.12.11
+
+// setup github and VS code at home
+let gameStart;
+let nausicaa;
+let ifGameStart = false;
+let runFront, runLeft, runRight, runBack;
+let gameMap;
+let ballAnimation = false;
+let ballAnimationWidth = 0;
+
+function setup() {
+  createCanvas(1024, 576);
+  // gameStart = loadAni("gameStart/IMG_01.png", 21);
+  nausicaa = new Sprite(250, 80, 120);
+  loadplayerRun();
+}
+
+function loadGameStart(){
+  nausicaa.addAni("gameStart", "imgS/Cat-2-Licking.png", { frameSize: [256, 256], frames: 5 });
+}
+
+function loadplayerRun() {
+  nausicaa.addAni("down", "playerRun/IMG_runFront.png", { frameSize: [256, 256], frames: 4 });
+  nausicaa.addAni("left", "playerRun/IMG_runLeft.png", { frameSize: [256, 256], frames: 4 });
+  nausicaa.addAni("right", "imgS/Cat-1-Run.png", { frameSize: [256, 256], frames: 8 });
+  nausicaa.addAni("up", "playerRun/IMG_runBack.png", { frameSize: [256, 256], frames: 4 });
+}
+
+function preload() {
+  // gameMap = loadImage("gameMap/gameMap.png");
+  textFont1 = loadFont('textFont/8-bitanco.ttf');
+}
+
+function draw() {
+  // clear();
+  // testingBall();
+  // ballClicked();
+  if (ifGameStart === false) {
+    animation(gameStart, 1194, 834);
+    pressStart();
+  }
+  else if (ifGameStart === true) {
+    image(gameMap, 0, 0);
+    playerRun();
+    testingBall();
+    nausicaa.debug = mouse.pressing();
+    if (kb.pressing("left")) {
+      nausicaa.changeAni("left");
+      nausicaa.x -= 2;
+    }
+    else if (kb.pressing("right")) {
+      nausicaa.changeAni("right");
+      nausicaa.x += 2;
+    }
+    else if (kb.pressing("down")) {
+      nausicaa.changeAni("down");
+      nausicaa.y += 2;
+    }
+    else if (kb.pressing("up")) {
+      nausicaa.changeAni("up");
+      nausicaa.y -= 2;
+    }
+    if (kb.pressing('space')) {
+      nausicaa.ani.stop();
+    }
+    else {
+      nausicaa.ani.play();
+    }
+  }
+}
+
+function playerRun() {
+  if (keyIsPressed === true && keyCode === DOWN_ARROW) {
+    scale(0.3);
+    animation(runFront, 2500, 2500);
+  }
+  else if (keyIsPressed === true && keyCode === UP_ARROW) {
+    scale(0.3);
+    animation(runBack, 2500, 2500);
+  }
+  else if (keyIsPressed === true && keyCode === LEFT_ARROW) {
+    scale(0.3);
+    animation(runLeft, 2500, 2500);
+  }
+  else if (keyIsPressed === true && keyCode === RIGHT_ARROW) {
+    scale(0.3);
+    animation(runRight, 2500, 2500);
+  }
+}
+
+function pressStart() {
+  fill(255);
+  textFont(textFont1, 60);
+  stroke(0);
+  strokeWeight(4);
+  text('Press ENTER to Start the Game', 1300, 1600);
+  if (ifGameStart === true) {
+    gameBegin();
+  }
+}
+
+function keyPressed() {
+  if (ifGameStart === false && keyCode === ENTER) {
+    ifGameStart = true;
+  }
+}
+
+// function testingBall() {
+//   fill(255);
+//   circle(200, 200, 100);
+// }
+
+// function ballClicked() {
+//   if (keyPressed && keyCode === ENTER) {
+//     fill(255, 0, 0);
+//     rect(100, 100, 200, 10);
+//   }
+//   if (keyIsPressed && keyCode === CONTROL && ballAnimation === false) {
+//     if (ballAnimationWidth <= 0) {
+//       ballAnimationWidth = 200;
+//     }
+//     else if (ballAnimationWidth > 0) {
+//       ballAnimationWidth -= 5;
+//     }
+//     else {
+//       ballAnimation = 0;
+//     }
+//   }
+//   fill(255, 0, 0);
+//   rect(100, 100, ballAnimationWidth, 10);
+// }
--- Capstone Project, Jillian/tryout.js
+++ Capstone Project, Jillian/tryout.js
@@ -1,130 +1,137 @@
-// Nausicaa
-// Jillian
-// 2023.12.11
-
-// setup github and VS code at home
-let gameStart;
-let nausicaa;
-let ifGameStart = false;
-let runFront, runLeft, runRight, runBack;
-let gameMap;
-let ballAnimation = false;
-let ballAnimationWidth = 0;
-
-function setup() {
-  createCanvas(2388, 1668);
-  gameStart = loadAni("gameStart/IMG_01.png", 21);
-  nausicaa = new Sprite(250, 80, 120);
-  loadplayerRun();
-}
-
-function loadplayerRun() {
-  nausicaa.addAni("down", "playerRun/IMG_runFront.png", { frameSize: [256, 256], frames: 4 });
-  nausicaa.addAni("left", "playerRun/IMG_runLeft.png", { frameSize: [256, 256], frames: 4 });
-  nausicaa.addAni("right", "playerRun/IMG_runRight.png", { frameSize: [256, 256], frames: 4 });
-  nausicaa.addAni("up", "playerRun/IMG_runBack.png", { frameSize: [256, 256], frames: 4 });
-}
+let cat1;
 
 function preload() {
-  gameMap = loadImage("gameMap/gameMap.png");
-  textFont1 = loadFont('textFont/8-bitanco.ttf');
+  // Load your image before setup
+  cat1 = loadImage("imgS/Cat-2-Licking.png");
+}
+
+function setup() {
+  createCanvas(1024, 576);
+  // Create a sprite using createSprite
+  cat1 = createSprite(256, 256, 120, 120);
+  // Add animation to the sprite
+  cat1.addAnimation("gameStart", cat1);
+  // Set the animation frames
+  cat1.animation.frameDelay = 200; // Adjust the frame delay as needed
 }
 
 function draw() {
-  // clear();
-  testingBall();
-  ballClicked();
-  if (ifGameStart === false) {
-    animation(gameStart, 1194, 834);
-    pressStart();
-  }
-  else if (ifGameStart === true) {
-    image(gameMap, 0, 0);
-    playerRun();
-    testingBall();
-    nausicaa.debug = mouse.pressing();
-    if (kb.pressing("left")) {
-      nausicaa.changeAni("left");
-      nausicaa.x -= 2;
-    }
-    else if (kb.pressing("right")) {
-      nausicaa.changeAni("right");
-      nausicaa.x += 2;
-    }
-    else if (kb.pressing("down")) {
-      nausicaa.changeAni("down");
-      nausicaa.y += 2;
-    }
-    else if (kb.pressing("up")) {
-      nausicaa.changeAni("up");
-      nausicaa.y -= 2;
-    }
-    if (kb.pressing('space')) {
-      nausicaa.ani.stop();
-    }
-    else {
-      nausicaa.ani.play();
-    }
-  }
+  background(255);
+  // Display the sprite
+  drawSprites();
 }
 
-function playerRun() {
-  if (keyIsPressed === true && keyCode === DOWN_ARROW) {
-    scale(0.3);
-    animation(runFront, 2500, 2500);
-  }
-  else if (keyIsPressed === true && keyCode === UP_ARROW) {
-    scale(0.3);
-    animation(runBack, 2500, 2500);
-  }
-  else if (keyIsPressed === true && keyCode === LEFT_ARROW) {
-    scale(0.3);
-    animation(runLeft, 2500, 2500);
-  }
-  else if (keyIsPressed === true && keyCode === RIGHT_ARROW) {
-    scale(0.3);
-    animation(runRight, 2500, 2500);
-  }
-}
 
-function pressStart() {
-  fill(255);
-  textFont(textFont1, 60);
-  stroke(0);
-  strokeWeight(4);
-  text('Press ENTER to Start the Game', 1300, 1600);
-  if (ifGameStart === true) {
-    gameBegin();
-  }
-}
+// let cat1;
+// // let ifGameStart = false;
+// // let runFront, runLeft, runRight, runBack;
 
-function keyPressed() {
-  if (ifGameStart === false && keyCode === ENTER) {
-    ifGameStart = true;
-  }
-}
+// function preload(){
+//   cat1 = loadImage("imgS/Cat-2-Licking.png");
+// }
 
-function testingBall() {
-  fill(255);
-  circle(200, 200, 100);
-}
+// function setup() {
+//   createCanvas(1024, 576);
+//   cat1 = new Sprite(250, 80, 120);
+//   // loadplayerRun();
+//   loadGameStart();
+// }
 
-function ballClicked() {
-  if (keyPressed && keyCode === ENTER) {
-    fill(255, 0, 0);
-    rect(100, 100, 200, 10);
-  }
-  if (keyIsPressed && keyCode === CONTROL && ballAnimation === false) {
-    if (ballAnimationWidth <= 0) {
-      ballAnimationWidth = 200;
-    }
-    else if (ballAnimationWidth > 0) {
-      ballAnimationWidth -= 5;
-    }
-    else {
-      ballAnimation = 0;
-    }
-  }
-  fill(255, 0, 0);
-  rect(100, 100, ballAnimationWidth, 10);
-}
+// // function setup() {
+// //   createCanvas(1024, 576);
+// //   cat1 = new Sprite(250, 80, 120);
+// //   loadplayerRun();
+// // }
+
+// function loadGameStart(){
+//   cat1.addAni("gameStart", "imgS/Cat-2-Licking.png", { frameSize: [256, 256], frames: 5 });
+// }
+
+// function draw() {
+//   background(255);
+//   animation(gameStart, 256, 256);
+// }
+
+// function loadplayerRun() {
+//   cat1.addAni("down", "playerRun/IMG_runFront.png", { frameSize: [256, 256], frames: 4 });
+//   cat1.addAni("left", "playerRun/IMG_runLeft.png", { frameSize: [256, 256], frames: 4 });
+//   cat1.addAni("right", "imgS/Cat-1-Run.png", { frameSize: [256, 256], frames: 8 });
+//   cat1.addAni("up", "playerRun/IMG_runBack.png", { frameSize: [256, 256], frames: 4 });
+// }
+
+// // function preload() {
+// //   // gameMap = loadImage("gameMap/gameMap.png");
+// //   textFont1 = loadFont('textFont/8-bitanco.ttf');
+// // }
+
+
+// function draw() {
+//   background(0);
+//   // clear();
+//   if (ifGameStart === false) {
+//     animation(gameStart, 256, 256);
+//     pressStart();
+//   }
+//   else if (ifGameStart === true) {
+//     playerRun();
+//     cat1.debug = mouse.pressing();
+//     if (kb.pressing("left")) {
+//       cat1.changeAni("left");
+//       cat1.x -= 2;
+//     }
+//     else if (kb.pressing("right")) {
+//       cat1.changeAni("right");
+//       cat1.x += 2;
+//     }
+//     else if (kb.pressing("down")) {
+//       cat1.changeAni("down");
+//       cat1.y += 2;
+//     }
+//     else if (kb.pressing("up")) {
+//       cat1.changeAni("up");
+//       cat1.y -= 2;
+//     }
+//     if (kb.pressing('space')) {
+//       cat1.ani.stop();
+//     }
+//     else {
+//       cat1.ani.play();
+//     }
+//   }
+// }
+
+// function playerRun() {
+//   if (keyIsPressed === true && keyCode === DOWN_ARROW) {
+//     scale(0.3);
+//     animation(runFront, 2500, 2500);
+//   }
+//   else if (keyIsPressed === true && keyCode === UP_ARROW) {
+//     scale(0.3);
+//     animation(runBack, 2500, 2500);
+//   }
+//   else if (keyIsPressed === true && keyCode === LEFT_ARROW) {
+//     scale(0.3);
+//     animation(runLeft, 2500, 2500);
+//   }
+//   else if (keyIsPressed === true && keyCode === RIGHT_ARROW) {
+//     scale(0.3);
+//     animation(runRight, 2500, 2500);
+//   }
+// }
+
+// function pressStart() {
+//   fill(255);
+//   textFont(textFont1, 60);
+//   stroke(0);
+//   strokeWeight(4);
+//   text('Press ENTER to Start the Game', 100, 150);
+//   if (ifGameStart === true) {
+//     gameBegin();
+//   }
+// }
+
+// function keyPressed() {
+//   if (ifGameStart === false && keyCode === ENTER) {
+//     ifGameStart = true;
+//   }
+// }
